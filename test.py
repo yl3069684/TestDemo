9370da226dc9adc65d834ec458abbfdf103cce90
yanlong123 <549809183@qq.com>
2020-04-17 14:09:44 +0800

    allure

--- test.py
+++ test.py
@@ -1,5 +1,27 @@
-def func(x):
-    return x +1
+import allure
+import pytest
 
-def test_answer():
-    assert func(4)==5
+@allure.step("步骤1:点xxx")
+def step_1():
+    print("111")
+
+@allure.step("步骤2:点xxx")
+def step_2():
+    print("222")
+
+@allure.feature("编辑页面")
+class TestEditPage():
+    '''编辑页面'''
+
+    @allure.story("这是一个xxx的用例")
+    def test_1(self, login):
+        '''用例描述：先登录，再去执行xxx'''
+        step_1()
+        step_2()
+        print("xxx")
+
+
+    @allure.story("打开a页面")
+    def test_2(self, login):
+        '''用例描述：先登录，再去执行yyy'''
+        print("yyy")
